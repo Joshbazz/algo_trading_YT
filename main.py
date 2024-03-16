2eaf9ea773006c0e0545e3013e10c579b82100d9
Joshbazz <joshbazz36@gmail.com>
2024-03-16 19:35:24 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+ticker = ["SPY", "AAPL", "KO"]
+
+stocks = yf.download(ticker, start = "2010-01-01", end = "2021-01-01")
+
+stocks = pd.read_csv("stocksYT.csv", header = [0,1], index_col=[0], parse_dates=[0])
+
+# convert multi index to tuple
+#stocks.columns=stocks.columns.to_flat_index()
+
+close = stocks.loc[:, "Close"].copy()
