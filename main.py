50e44b7846424a140063bfa7362d003c3f3f3ce8
Joshbazz <joshbazz36@gmail.com>
2024-03-16 22:24:21 -0400

    update main.py for code thru 31:34 video time

--- main.py
+++ main.py
@@ -13,3 +13,9 @@ stocks = pd.read_csv("stocksYT.csv", header = [0,1], index_col=[0], parse_dates=
 #stocks.columns=stocks.columns.to_flat_index()
 
 close = stocks.loc[:, "Close"].copy()
+
+normal_close = close.div(close.iloc[0]).mul(100)
+
+normal_close.plot(figsize=(15,8), fontsize=12)
+plt.legend(fontsize=12)
+plt.show()
